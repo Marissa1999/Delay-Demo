49cc3d365cba185c8c6ec2a44f26b22ccfc0845b
Marissa Gonçalves <marissagoncalves13@gmail.com>
2021-01-05 09:34:00 -0500

    Initial Commit.

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function()
+{
+	
+	("button").click(function()
+	{
+		
+		$("#div1").fadeIn(400).delay(800);
+		$("#div2").fadeIn(400).delay(800);
+		$("#div3").fadeIn(400).delay(800);
+
+	});
+
+});
